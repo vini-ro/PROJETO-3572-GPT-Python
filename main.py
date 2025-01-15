51307dddb7a773b9c1a9204a494933cba592a3c8
Vinicius <vroliveira.eng@gmail.com>
2025-01-15 18:02:53 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+resposta = cliente.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "Listar apenas os nomes dos produtos, sem considerar descrição."},
+        {"role": "user", "content": "Liste 3 produtos sustentáveis."},
+    ],
+    model="gpt-4",
+)
+
+print (resposta.choices[0].message.content)

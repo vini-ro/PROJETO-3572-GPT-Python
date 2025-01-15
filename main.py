06520b91807ab10b9ec5877184d6fbd4b51ac182
Vinicius <vroliveira.eng@gmail.com>
2025-01-15 18:52:19 -0300

    primeiros text requests

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 resposta = cliente.chat.completions.create(
     messages=[
-        {"role": "system", "content": "Listar apenas os nomes dos produtos, sem considerar descrição."},
-        {"role": "user", "content": "Liste 3 produtos sustentáveis."},
+        {
+            "role":"system",
+            "content": """
+            Classifique o produto abaixo em uma das categorias: Higiene Pessoal, Moda ou Casa. De uma descrição da categoria.
+            """
+        },
+        {
+            "role" : "user",
+            "content": """
+            Escova de dentes de bambu
+            """
+        }
     ],
     model="gpt-4",
+    temperature=0,
+    max_tokens=200,
+    n = 3
 )
-
-print (resposta.choices[0].message.content)
+for contador in range(0,3):
+    print(resposta.choices[contador].message.content)
